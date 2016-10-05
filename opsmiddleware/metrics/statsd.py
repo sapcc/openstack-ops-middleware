1fd9cf5f447a5d5738f2ffef626bef95f14d081d
Falk Reimann <falk.reimann@sap.com>
2016-10-05 10:46:15 +0200

    Initial commit to enable swift specific path parsing

--- opsmiddleware/metrics/statsd.py
+++ opsmiddleware/metrics/statsd.py
@@ -4,15 +4,15 @@ import os
 import time
 import re
 import urlparse
+from abc import ABCMeta, abstractmethod
 from os.path import splitext
-
-ID_REGEX = re.compile(r'/[0-9a-fA-F-]+/')
 
 
 class StatsdMiddleware(object):
     def __init__(self, app, statsd_host='localhost', statsd_port='8125',
-                 statsd_prefix='openstack'):
+                 statsd_prefix='openstack', statsd_replace='id'):
         self.app = app
+        self._get_replace_strategy(os.getenv('STATSD_REPLACE', statsd_replace).lower())
         self.client = DogStatsd(
             host=os.getenv('STATSD_HOST', statsd_host),
             port=int(os.getenv('STATSD_PORT', statsd_port)),
@@ -44,8 +44,8 @@ class StatsdMiddleware(object):
         # strip extensions
         path = splitext(path)[0]
 
-        # replace identifiers with constant
-        path = ID_REGEX.sub('/id/', path + '/')
+        # replace parts of the path with constants based on strategy
+        path = self.replace_strategy.replace(path)
 
         parts = path.rstrip('\/').split('/')
         if exception:
@@ -96,3 +96,48 @@ class StatsdMiddleware(object):
             raise
         finally:
             self.client.close_buffer()
+
+    def _get_replace_strategy(self, strategy):
+        if strategy == 'swift':
+            self.replace_strategy = _ReplaceStrategySwift()
+        else:
+            self.replace_strategy = _ReplaceStrategyId()
+
+
+class _ReplaceStrategy(metaclass=ABCMeta):
+    @abstractmethod
+    def replace(self, path):
+        pass
+
+
+class _ReplaceStrategyId(_ReplaceStrategy):
+    def __init__(self):
+        self._regex = re.compile(r'/[0-9a-fA-F-]+/')
+
+    def replace(self, path):
+        # replace identifiers with constant
+        return self._regex.sub('/id/', path + '/')
+
+
+class _ReplaceStrategySwift(_ReplaceStrategy):
+    def __init__(self):
+        self._regex = re.compile(r'(\S+AUTH_)([p\-]?[0-9a-fA-F-]+/?)([^ /\t\n\r\f\v]+/?)?(\S*)?')
+
+    def replace(self, path):
+        # Transform Swift path from
+        # /v1/AUTH_0123456789/container-name/pseudo-folder/object-name
+        # to constants
+        # /v1/AUTH_account/container/object
+        m = self._regex.match(path)
+        if m:
+            # Replace account id with constant
+            path = m.group(1) + 'account'
+            if m.group(3) and m.group(3) != '':
+                # Replace container name with constant
+                path += '/container'
+            if m.group(4) and m.group(4) != '':
+                # Replace object name with constant
+                path += '/object'
+            path += '/'
+
+        return path

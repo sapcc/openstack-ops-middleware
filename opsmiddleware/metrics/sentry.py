5c37cb586c50f4b467a06f0893fce477a006af61
Rudolf Vriend <rudolf.vriend@sap.com>
2018-03-08 09:41:19 +0100

    swallow sentry client instantiation issues

--- opsmiddleware/metrics/sentry.py
+++ opsmiddleware/metrics/sentry.py
@@ -5,6 +5,8 @@ from raven.conf import setup_logging
 from raven.handlers.logging import SentryHandler
 from raven.middleware import Sentry
 
+logger = logging.getLogger('raven')
+
 
 class SentryMiddleware(Sentry):
     def __init__(self, application, client=None):
@@ -13,9 +15,13 @@ class SentryMiddleware(Sentry):
     @classmethod
     def factory(cls, global_config, **local_config):
         def _factory(app):
-            client = Client(**local_config)
-            handler = SentryHandler(client, **local_config)
-            setup_logging(handler)
+            try:
+                client = Client(**local_config)
+                handler = SentryHandler(client, **local_config)
+                setup_logging(handler)
+            except Exception as e:
+                logger.error(e)
+                client = None
             return cls(app, client)
 
         return _factory
